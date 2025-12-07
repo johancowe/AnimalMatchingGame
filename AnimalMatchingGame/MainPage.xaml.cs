ea9754555d41873657785eb6b51b8ea557470395
Johan Cowe <johancowe@hotmail.com>
2025-12-07 02:24:45 +0100

    Initial commit: Animal Matching Game MAUI app

---
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace AnimalMatchingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            // TODO: Add logic to reset the game or start a new game
+            AnimalButtons.IsVisible = true;
+            PlayAgainButton.IsVisible = false;
+
+            List<String> animalEmoji = [
+                "🐙","🐙",
+                "🐁","🐁",
+                "🐄","🐄",
+                "🐈‍","🐈‍",
+                "🐍","🐍",
+                "🦆","🦆",
+                "🐧","🐧",
+                "🐟","🐟",
+             ];
+
+            foreach (var button in AnimalButtons.Children.OfType<Button>())
+            {
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+
+            }
+
+            Dispatcher.StartTimer(TimeSpan.FromSeconds(.1), TimerTick);
+        }
+
+        int tenthsOfSecondsElapsed = 0;
+
+        private bool TimerTick()
+        {
+            if (!this.IsLoaded) return false;
+
+            tenthsOfSecondsElapsed++;
+
+            TimeElapsed.Text = "Time elapsed: " + (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+
+            if (PlayAgainButton.IsVisible)
+            {
+                tenthsOfSecondsElapsed = 0;
+                return false;
+            }
+
+            return true;
+
+        }
+
+
+
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button buttonClicked)
+            {
+
+                if (!string.IsNullOrEmpty(buttonClicked.Text) && (findingMatch == false))
+                {
+                    buttonClicked.BackgroundColor = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text) && (!String.IsNullOrWhiteSpace(buttonClicked.Text)))
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+            }
+
+            if (matchesFound == 8)
+            {
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+                matchesFound = 0;
+            }
+
+
+        }
+    }
+}
